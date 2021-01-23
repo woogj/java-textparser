bf89926dce353ca5417bd71297143cf5fe3e92ae
woogj <delay0316@naver.com>
2021-01-23 16:12:49 +0800

    Refactor Main
    Refactor Main to input text type

---
+++ src/com/mirae/parsing/ParsingExcel.java
@@ -0,0 +1,47 @@
+package com.mirae.parsing;
+
+
+import com.mirae.parsing.utils.ReadExcel;
+import com.mirae.parsing.utils.WriteExcel;
+import com.mirae.parsing.utils.getObjectSize;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class ParsingExcel {
+    private static Logger log = LoggerFactory.getLogger(ParsingExcel.class);
+    private static long startTm = 0L;
+    private static long endTm = 0L;
+
+    public static void parsingExcel(String filename){
+
+        Date date = new Date();
+
+        SimpleDateFormat forExcel = new SimpleDateFormat("yyMMddHHmmss");
+        SimpleDateFormat forLog = new SimpleDateFormat("mm:ss:S");
+        startTm = System.currentTimeMillis();
+        log.debug("Excel file parsing starting");
+
+        ArrayList<String[]> excel = ReadExcel.getExcelList(filename);
+        String[] no = excel.get(0);
+        String[] excelFile = excel.get(1);
+        ArrayList<ArrayList<String>> setences = new ArrayList<>();
+        ArrayList<ArrayList<String>> rsSetences = new ArrayList<>();
+        BreakIterator boundary = BreakIterator.getSentenceInstance(Locale.KOREA);
+        setences = GetRefined.cropLine(excelFile);
+
+        for(ArrayList<String> Index: setences) {
+            rsSetences.add(GetRefined.Reattach(GetRefined.confirmSentence(GetBreakIterator.printEachForward(boundary, Index))));
+        }
+        log.debug("TextParsing terminated- total text: "+rsSetences.size()+", total sentences: "+ getObjectSize.getSizeObj(rsSetences));
+
+        WriteExcel.exportExcel(no, excelFile, rsSetences, filename+"_parsed"+forExcel.format(date));
+        endTm = System.currentTimeMillis();
+        log.debug(String.format("@@ TextParsing Terminated- %s (%s)", getObjectSize.getSize(rsSetences), forLog.format(endTm-startTm)));
+    }
+}
--- src/com/mirae/parsing/TextParsingMain.java
+++ src/com/mirae/parsing/TextParsingMain.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,39 +20,30 @@ public class TextParsingMain {
 	private static Logger log = LoggerFactory.getLogger(TextParsingMain.class);
 	private static long startTm = 0L;
 	private static long endTm = 0L;
-	
+
 	public static void main(String[] args) {
 		Date date = new Date();
-		
+
 		SimpleDateFormat forExcel = new SimpleDateFormat("yyMMddHHmmss");
 		SimpleDateFormat forLog = new SimpleDateFormat("mm:ss:S");
 		startTm = System.currentTimeMillis();
 		log.debug("@@ TextParsing Started");
 		Scanner sc =new Scanner(System.in);
-		System.out.println("Input Excel 파일 명을 입력해주세요. (파일명을 입력하지 않으신 경우 conf/information.xml에 설정되어 있는 filename이 입력됩니다.)");
+		System.out.println("파싱하실 구문 혹은 Excel 파일 명(파일이름.확장자)을 입력해주세요. (파일명을 입력하지 않으신 경우 conf/information.xml에 설정되어 있는 filename이 입력됩니다.)");
 		String str = sc.nextLine();
 		String filename ="";
+		String pattern = ".*\\.xls[x]?";
+
+
 		if(str.isEmpty()) {
 //			conf/information.xml에서 설정된 파일명 가져오기
 			filename = DOMParser.getFilename();
-		}else {
+			ParsingExcel.parsingExcel(filename);
+		}else if(str.matches(pattern)){
 			filename = str;
+			ParsingExcel.parsingExcel(filename);
+		}else{
+			ParsingText.parsingText(str);
 		}
-		ArrayList<String[]> excel = ReadExcel.getExcelList(filename);
-		String[] no = excel.get(0);
-		String[] excelFile = excel.get(1);
-		ArrayList<ArrayList<String>> setences = new ArrayList<>();
-		ArrayList<ArrayList<String>> rsSetences = new ArrayList<>();
-		BreakIterator boundary = BreakIterator.getSentenceInstance(Locale.KOREA);
-		setences = GetRefined.cropLine(excelFile);
-		
-		for(ArrayList<String> Index: setences) {
-			rsSetences.add(GetRefined.Reattach(GetRefined.confirmSentence(GetBreakIterator.printEachForward(boundary, Index))));
-		}
-		log.debug("TextParsing terminated- total text: "+rsSetences.size()+", total sentences: "+getObjectSize.getSizeObj(rsSetences));
-		
-		WriteExcel.exportExcel(no, excelFile, rsSetences, filename+"_parsed"+forExcel.format(date));
-		endTm = System.currentTimeMillis();
-		log.debug(String.format("@@ TextParsing Terminated- %s (%s)", getObjectSize.getSize(rsSetences), forLog.format(endTm-startTm)));
 	}
 }
